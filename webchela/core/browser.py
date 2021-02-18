cdc0e422513d64f6f90210f921394b25b9f484f5
livelace <o.popov@livelace.ru>
2021-02-18 17:33:40 +0300

    v1.5.0

--- webchela/core/browser.py
+++ webchela/core/browser.py
@@ -56,6 +56,7 @@ class Browser:
 
         if self.request.browser.proxy:
             self.selenium_wire_options = {
+                "backend": "mitmproxy",
                 "proxy": {
                     "http": "{}".format(self.request.browser.proxy),
                     "https": "{}".format(self.request.browser.proxy),
