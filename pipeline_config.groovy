559c74203c908c58af42b87c45bb8fc57a58735e
livelace <o.popov@livelace.ru>
2021-07-27 06:27:54 +0300

    README.md

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,19 +1,14 @@
-jte {
-    pipeline_template = 'k8s_build.groovy'
-}
-
 libraries {
     git {
-        repo_url = 'https://github.com/livelace/webchela.git'
+        repo_url = "https://github.com/livelace/webchela.git"
+    }
+    k8s {
+        image = "harbor-core.k8s-2.livelace.ru/infra/service-core:latest"
     }
     mattermost
     nexus {
-       source = 'dist/webchela-1.5.2-py3.8.egg'
-       destination = 'dists-internal/webchela/webchela-1.5.2-py3.8.egg'
+       source = "dist/webchela-1.5.2-py3.8.egg"
+       destination = "dists-internal/webchela/webchela-1.5.2-py3.8.egg"
     }
     python
-}
-
-keywords {
-    build_image = 'harbor-core.k8s-2.livelace.ru/infra/service-core:latest'
 }
