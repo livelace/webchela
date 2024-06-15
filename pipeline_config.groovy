161824510fc83c1b7922edf73b9c2efc9a857eb0
Oleg 'livelace' Popov <o.popov@livelace.ru>
2024-06-15 03:31:03 +0300

    ci refactor fix vulnerabilities

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,7 +4,7 @@ def APP_VERSION = env.VERSION + '-${GIT_COMMIT_SHORT}'
 def IMAGE_TAG = env.VERSION == "master" ? "latest" : env.VERSION
 
 libraries {
-    dependency_check
+    //dependency_check
     //dependency_track {
     //    project = "${APP_NAME}"
     //    version = env.VERSION
@@ -13,21 +13,19 @@ libraries {
         repo_url = "${APP_REPO}"
         repo_branch = env.VERSION
     }
-    harbor_replicate {
-        policy = "${APP_NAME}"
-    }
-    k8s_build {
-        image = "harbor-core.k8s-2.livelace.ru/infra/tools:latest"
-    }
+    //harbor_replicate {
+    //    policy = "${APP_NAME}"
+    //}
+    k8s_build
     kaniko {
-        destination = "data/${APP_NAME}:${IMAGE_TAG}"
+        destination = "infra/${APP_NAME}:${IMAGE_TAG}"
     }
     mattermost
-    nexus {
-       source = "dist/webchela-1.7.0-py3.8.egg"
-       destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.egg"
+    //nexus {
+    //   source = "dist/webchela-1.7.0-py3.8.egg"
+    //   destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.egg"
 
-    }
+    //}
     python
-    sonarqube
+    //sonarqube
 }
